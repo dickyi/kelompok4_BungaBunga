fa21c3cd0c60e55b603b8d07855f37c7811a7f5c
M.Rizal <rizandi778@gmail.com>
2024-09-28 09:54:42 +0700

    Add Bunga Edelweis

--- src/Bunga/Main.java
+++ src/Bunga/Main.java
@@ -26,6 +26,21 @@ public class Main {
         System.out.println("Iklim             : " + bunga.getIklim());
         System.out.println("Tanah             : " + bunga.getTanah());
         System.out.println("Hewan Penyerbuk   : " + bunga.getHewan_penyerbuk());
+        
+        Bunga_Edelweis fr = new Bunga_Edelweis();
+
+        fr.setnama_ilmiah("Leontopolium alpinum");
+        fr.setWarna("Putih atau keperakan ditengahnya kuning");
+        fr.setIklim("Sejuk hingga dingin");
+        fr.setTanah("Berbatu");
+        fr.setHewan_penyerbuk("Lebah dan kupu-kupu");
+
+        System.out.println("\n\t\t\tBunga Edelweis");
+        System.out.println("Nama Ilmiah       : " + fr.getnama_ilmiah());
+        System.out.println("Warna             : " + fr.getWarna());
+        System.out.println("Iklim             : " + fr.getIklim());
+        System.out.println("Tanah             : " + fr.getTanah());
+        System.out.println("Hewan Penyerbuk   : " + fr.getHewan_penyerbuk());
     }
 
 }
