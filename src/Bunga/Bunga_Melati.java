8a48a6550680709a39b5cf74c0e90963ce21d862
dickyi <irawandicky300@gmail.com>
2024-10-12 19:54:03 +0700

    Penerapan inheritance baru

--- src/Bunga/Bunga_Melati.java
+++ src/Bunga/Bunga_Melati.java
@@ -9,47 +9,8 @@ package Bunga;
  * @author Irawa
  */
 public class Bunga_Melati {
-    String nama_ilmiah, warna, iklim, tanah, hewan_penyerbuk;
-    
-    public void setnama_ilmiah(String namailmiah){
-        this.nama_ilmiah = namailmiah;
-    }
-    
-      public String getnama_ilmiah(){
-        return  nama_ilmiah;
-    }
-         public void setWarna(String warna) {
-        this.warna = warna;
-    }
-
-    public String getWarna() {
-        return warna;
-    }
-
    
-    public void setIklim(String iklim) {
-        this.iklim = iklim;
-    }
-
-    public String getIklim() {
-        return iklim;
-    }
-
-    
-    public void setTanah(String tanah) {
-        this.tanah = tanah;
-    }
-
-    public String getTanah() {
-        return tanah;
-    }
-
-    public void setHewan_penyerbuk(String hewanPenyerbuk) {
-        this.hewan_penyerbuk = hewanPenyerbuk;
-    }
-
-    public String getHewan_penyerbuk() {
-        return hewan_penyerbuk;
-    }
+    //aroma, penggunaan_tradisional;
+  
 
 }
